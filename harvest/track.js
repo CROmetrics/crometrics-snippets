7e9ef0fcdbb5324d7b9d0a803719ae3811bb1dfd
Garan Guillory <garanguillory@gmail.com>
2017-10-24 15:29:31 -0500

    Harvest - Track: Poll for jQuery.

--- harvest/track.js
+++ harvest/track.js
@@ -1,40 +1,47 @@
 (function () {
 
-  setTimeout(function(){
+    var script_jQuery=document.createElement('script');
+    script_jQuery.setAttribute('src','//code.jquery.com/jquery-latest.min.js');
+    document.body.appendChild(script_jQuery);
+    
+    (function poll(){
+      if(!window.jQuery) setTimeout(poll, 50);
+    
+      var clientsTable = $('#clients-table').children('tbody');
+      console.log(clientsTable);
 
-    var clientsTable = $('#clients-table').children('tbody');
-    console.log(clientsTable);
+      if (clientsTable.length > 0) {
 
-    if (clientsTable.length > 0) {
+        // Insert a New "Profitability" Column
+        $('#clients-table th.td-item').after('<th class="td-profitability"><span>Profitability</span></th>');
+        // Insert the Profitability Cell
+        $('#clients-table td.td-item').after('<td class="td-profitability"></td>');
 
-      // Insert a New "Profitability" Column
-      $('#clients-table th.td-item').after('<th class="td-profitability"><span>Profitability</span></th>');
-      // Insert the Profitability Cell
-      $('#clients-table td.td-item').after('<td class="td-profitability"></td>');
+        clientsTable.each(function(i){
+          var thisClient = $(this);
+          var thisClientName = thisClient.find('.td-item a').text();
 
-      clientsTable.each(function(i){
-        var thisClient = $(this);
-        var thisClientName = thisClient.find('.td-item a').text();
+          if (clientNames.indexOf(thisClientName) > -1) {
+            var beginDate = getParam('from');
+            var endDate = getParam('till');
+          var a = moment([Number(beginDate.substr(0,4)), Number(beginDate.substr(4,2))]);
+          var b = moment([Number(endDate.substr(0,4)), Number(endDate.substr(4,2))]);
+          var monthsBetween = Math.abs(b.diff(a, 'months'));
+          var thisClientHours = Number(thisClient.find('.td-hours a').text());
+            var thisClientProfitability = Math.floor((window.clientMonthly[thisClientName] * monthsBetween) / thisClientHours);
+            var thisClientProfitabilityStr = "$" + thisClientProfitability + " / hr";
+            thisClient.find('.td-profitability').text(thisClientProfitabilityStr);
+          }
+        });
 
-        if (clientNames.indexOf(thisClientName) > -1) {
-          var beginDate = getParam('from');
-          var endDate = getParam('till');
-        var a = moment([Number(beginDate.substr(0,4)), Number(beginDate.substr(4,2))]);
-        var b = moment([Number(endDate.substr(0,4)), Number(endDate.substr(4,2))]);
-        var monthsBetween = Math.abs(b.diff(a, 'months'));
-        var thisClientHours = Number(thisClient.find('.td-hours a').text());
-          var thisClientProfitability = Math.floor((window.clientMonthly[thisClientName] * monthsBetween) / thisClientHours);
-          var thisClientProfitabilityStr = "$" + thisClientProfitability + " / hr";
-          thisClient.find('.td-profitability').text(thisClientProfitabilityStr);
-        }
-      });
+      }
+      else {
+        console.log("[Harvest] Profitability View: Abort! Not a Client Report!");
+      }
 
-    }
-    else {
-      console.log("[Harvest] Profitability View: Abort! Not a Client Report!");
-    }
+    })();
 
-  }, 500);
+
 
   function getParam(name, url) {
 	    if (!url) {
