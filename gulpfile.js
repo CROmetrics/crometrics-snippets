c9e743bb6daefbaef8214c0fdbd0f9a0aec68007
Andrew Wessels <shixish@gmail.com>
2020-01-27 21:07:03 -1000

    Updating some deps, see if this works

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ gulp.task('compile', () => {
   const babel = require('gulp-babel');
   const rename = require('gulp-rename');
 
-  let compilation_options = {
-    minify: true
-  };
+  // let compilation_options = {
+  //   minify: true
+  // };
 
   return new Promise(function(resolve /*, reject*/ ) {
     gulp.src(['*/*.es6'])
       .pipe(babel({
-          presets: ['babili'],//'es2015', 
+          presets: ['minify'],//'es2015', 
           plugins: ['transform-es2015-template-literals']
       }))
       .pipe(rename({suffix: ".min",extname: '.js'}))
